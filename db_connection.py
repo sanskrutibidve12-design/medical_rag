f50ec1f75886cae13cfccd1806db701be05af2cc
sanskruti <sanskrutibidve12@gmail.com>
2026-03-25 00:25:25 +0530

    added

--- db_connection.py
+++ db_connection.py
@@ -1,10 +1,10 @@
 from supabase import create_client
-import os
+import streamlit as st
 from dotenv import load_dotenv
 
 load_dotenv()
 
-SUPABASE_URL = os.secrets("SUPABASE_URL")
-SUPABASE_KEY = os.secrets("SUPABASE_KEY")
+SUPABASE_URL = st.secrets("SUPABASE_URL")
+SUPABASE_KEY = st.secrets("SUPABASE_KEY")
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
